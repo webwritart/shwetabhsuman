d6016779190f7455027e21d052e5e027ffcab2f8
webwritart <139227135+webwritart@users.noreply.github.com>
2024-10-03 22:49:27 +0530

    Update member.py

--- models/member.py
+++ models/member.py
@@ -1,34 +1,34 @@
 # from extensions import db
-from flask_login import UserMixin
+# from flask_login import UserMixin
 
 
-member_role = db.Table('member_role',
-                       db.Column('member_id', db.Integer, db.ForeignKey('member.id')),
-                       db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
-                       )
+# member_role = db.Table('member_role',
+#                        db.Column('member_id', db.Integer, db.ForeignKey('member.id')),
+#                        db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+#                        )
 
 
-class Member(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    role = db.relationship('Role', secondary=member_role, backref='members')
-
-
-
-
-    def __repr__(self):
-        return f'{self.name}'
-
-
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.String(500))
+# class Member(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(100), unique=True)
+#     password = db.Column(db.String(100))
+#     name = db.Column(db.String(100))
+#     role = db.relationship('Role', secondary=member_role, backref='members')
 
 
 
 
-    def __repr__(self):
-        return f'{self.name}'
+#     def __repr__(self):
+#         return f'{self.name}'
+
+
+# class Role(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     description = db.Column(db.String(500))
+
+
+
+
+#     def __repr__(self):
+#         return f'{self.name}'
