6ff4114b96fe70f1f44f4055cd6c2d1c4b90e21f
webwritart <139227135+webwritart@users.noreply.github.com>
2024-10-04 07:57:13 +0530

    Update main_site.py

--- main_site.py
+++ main_site.py
@@ -1,14 +1,12 @@
 import os
 # from extensions import db, login_manager
-<<<<<<< HEAD
-from models.member import Member
-=======
+
+#from models.member import Member
 # from models.member import Member
->>>>>>> fbc20e2b7632053eea779d1148f902826ee6c261
 from routes.main import main
-from routes.gallery import gallery
+#from routes.gallery import gallery
 # from routes.account import account
-from routes.manager import manager
+#from routes.manager import manager
 from routes.about import about
 from flask import Flask
 # from dotenv import load_dotenv
@@ -27,22 +25,19 @@ app.secret_key = 'erghihogiOEIRTighrGHIER948ERGHr8g(%dgeoi*%J9HL(3grgfgr9KJJK'
 # ------------------------------ BLUEPRINTS -------------------------------- #
 
 app.register_blueprint(main, url_prefix='/')
-app.register_blueprint(gallery, url_prefix='/gallery')
+#app.register_blueprint(gallery, url_prefix='/gallery')
 # app.register_blueprint(account, url_prefix='/account')
-app.register_blueprint(manager, url_prefix='/manager')
+#app.register_blueprint(manager, url_prefix='/manager')
 app.register_blueprint(about, url_prefix='/about')
 
 # with app.app_context():
 #     db.create_all()
 
 
-<<<<<<< HEAD
 # @login_manager.user_loader
 # def load_user(member_id):
-=======
 # # @login_manager.user_loader
 # # def load_user(member_id):
->>>>>>> fbc20e2b7632053eea779d1148f902826ee6c261
 #     return db.get_or_404(Member, member_id)
 
 
